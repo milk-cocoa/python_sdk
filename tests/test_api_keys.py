277ff554a88ab3a4c8ce7501277a2d2e3cb08205
ervitis <vitomarti@gmail.com>
2015-12-12 17:51:33 +0100

    feat added some initial tests for api key

---
+++ tests/test_api_keys.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+
+import unittest
+
+
+class ApiKeys(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_file_exists(self):
+        pass
+
+    def test_has_good_header(self):
+        pass
+
+    def test_has_bad_header(self):
+        pass
+
+    def test_has_app_id(self):
+        pass
+
+    def test_has_access_key(self):
+        pass
+
+    def test_has_secret_key(self):
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
